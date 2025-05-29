9ec9ac0fb5b77b6e08a791fe519cc19f073076e8
zaynab-lb <zaynablboudaoudi@gmail.com>
2025-05-29 16:26:41 +0100

    add product api json file + internet permission

--- app/src/main/java/com/example/projetmobile/data/Api/ProductApi.kt
+++ app/src/main/java/com/example/projetmobile/data/Api/ProductApi.kt
@@ -4,6 +4,6 @@ import com.example.projetmobile.data.Entities.Product
 import retrofit2.http.GET
 
 interface ProductApi {
-    @GET("products")
+    @GET("products.json")
     suspend fun getProducts(): List<Product>
 }
